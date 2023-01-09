4686326de71aace7d3595f299a2735091518c8a9
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2023-01-09 10:48:10 +0200

    news_app

---
+++ lib/backend_notes.dart
@@ -0,0 +1,30 @@
+// In video Shared pref :-
+
+// 1- make one method to save all types of data (string , int , bool , double) in shared pref
+
+// 2- make one method to get all types of data (string , int , bool , double) in shared pref
+
+// 3- save data of onBoarding in shared pref when the user skip or when he reach last screen by using FloatingActionButton and go to login screen
+
+// 4- save data of token in shared pref when the user write his email and password correct in login screen and go to shop layout screen
+
+// if (onBoarding != null) {
+//   if (token != null) {
+//     widget = const ShopLayout();
+//   } else {
+//     widget = ShopLoginScreen();
+//   }
+// } else {
+//   widget = const OnBoardingScreen();
+// }
+
+// ================================================================================
+// In video Build Shop Layout [1] :-
+
+// 1- Build layout screen (bottomNavigationBar)
+
+// 2- build home model
+
+// 3- home is get
+
+// 4- call get method in cubit , give it the url and token , call it when the application start
